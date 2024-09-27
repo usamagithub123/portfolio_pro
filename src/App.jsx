f270ed63f40745df8bca8d9d8f6ef5c7a29aede7
usamagithub123 <usamaashraf540@gmail.com>
2024-09-27 17:29:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from './sections/Navbar'
+import Hero from './sections/Hero'
+import About from './sections/About'
+import Projects from './sections/Projects'
+import Contact from './sections/Contact'
+import Footer from './sections/Footer'
+
+const App = () => {
+  return (
+    <main className='max-w-7xl mx-auto'>
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Projects/>
+    
+      <Contact/>
+      <Footer/>
+    </main>
+  )
+}
+
+export default App
